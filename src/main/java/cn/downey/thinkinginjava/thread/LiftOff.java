a8a4b9f1fbd9928ca5774ebe390b31700270b146
downey <532720708@qq.com>
2019-11-24 08:42:37 +0800

    test

--- src/main/java/cn/downey/thinkinginjava/thread/LiftOff.java
+++ src/main/java/cn/downey/thinkinginjava/thread/LiftOff.java
@@ -16,7 +16,7 @@ public class LiftOff implements Runnable{
 
     public void run() {
         while(countDown-- > 0){
-            System.out.println(status());
+            System.out.println(Thread.currentThread()+"-----"+status());
             Thread.yield();
         }
     }
