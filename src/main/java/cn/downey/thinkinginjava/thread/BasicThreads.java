4ec29ba309dff109a24040b0fa90b62b4599df9b
downey <532720708@qq.com>
2019-11-24 08:23:16 +0800

    test

---
+++ src/main/java/cn/downey/thinkinginjava/thread/BasicThreads.java
@@ -0,0 +1,9 @@
+package cn.downey.thinkinginjava.thread;
+
+public class BasicThreads {
+    public static void main(String[] args) {
+        Thread t = new Thread(new LiftOff());
+        t.start();
+        System.out.println("Waiting for Liftoff");
+    }
+}
---
+++ src/main/java/cn/downey/thinkinginjava/thread/MainThread.java
@@ -0,0 +1,8 @@
+package cn.downey.thinkinginjava.thread;
+
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff liftOff = new LiftOff();
+        liftOff.run();
+    }
+}
