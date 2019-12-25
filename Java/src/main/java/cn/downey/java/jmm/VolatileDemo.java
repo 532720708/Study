7ced403760b7b0b80b82417eeb76ba561708712e
downey <532720708@qq.com>
2019-12-25 20:34:10 +0800

    Volatile不保证原子性

--- Java/src/main/java/cn/downey/java/jmm/VolatileDemo.java
+++ Java/src/main/java/cn/downey/java/jmm/VolatileDemo.java
@@ -9,16 +9,46 @@ class MyData {
         this.number = 60;
     }
 
+    public void addPlusPlus() {
+        number++;
+    }
+
 }
 
 /**
  * 1   验证volatile的可见性
  * 1.1  假如 int number = 0; number变量之前根本没有添加volatile关键字，没有可见性
+ * 1.2  添加了volatile，可以解决可见性问题
+ * <p>
+ * 2 验证volatile不保证原子性
+ * 2.1  原子性指的是什么意思？
+ * 不可分割，完整性，也即某个线程正在做某个具体业务时，中间不可以被加塞或者是被分割。
+ * 需要整体完整，要么同时成功，要么同时失败。
+ * 2.2 volatile不保证原子性
+ * 2.3 为什么不保证原子性
  */
 public class VolatileDemo {
 
     public static void main(String[] args) {
+        MyData myData = new MyData();
+        for (int i = 1; i <= 20; i++) {
+            new Thread(() -> {
+                for (int j = 1; j <= 1000; j++) {
+                    myData.addPlusPlus();
+                }
+            }, String.valueOf(i)).start();
+        }
 
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t finally number value: " + myData.number);
+    }
+
+    /**
+     * volatile可以保证可见性，及时通知其它线程，主物理内存的值已经被修改。
+     */
+    private static void seeOkByVolatile() {
         MyData myData = new MyData();
 
         new Thread(() -> {
