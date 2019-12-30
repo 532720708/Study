c5958267a40b60f8aa5b14e0e353b62b79899a0a
downey <532720708@qq.com>
2019-12-30 17:47:38 +0800

    1230

--- Java/src/main/java/cn/downey/java/jvm/GCRootDemo.java
+++ Java/src/main/java/cn/downey/java/jvm/GCRootDemo.java
@@ -10,11 +10,15 @@ package cn.downey.java.jvm;
 public class GCRootDemo {
     private byte[] byteArray = new byte[100 * 1024 * 1024];
 
+    //private static GCRootDemo2 t2;
+    //private static final GCRootDemo3 t3 = new GCRootDemo3(8)
+
     public static void m1() {
         GCRootDemo t1 = new GCRootDemo();
         System.gc();
         System.out.println("第一次GC完成");
     }
+
 
     public static void main(String[] args) {
         m1();
