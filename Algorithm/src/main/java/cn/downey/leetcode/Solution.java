d792af01998c44c823879665cfd929aad51115be
downey <532720708@qq.com>
2020-01-04 16:01:53 +0800

    isPowerOfFour

--- Algorithm/src/main/java/cn/downey/leetcode/Solution.java
+++ Algorithm/src/main/java/cn/downey/leetcode/Solution.java
@@ -243,4 +243,14 @@ class Solution {
         return arr[0];
     }
 
+    public boolean isPowerOfFour(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num % 4 == 0) {
+            num /= 4;
+        }
+        return num == 1;
+    }
+
 }
