d1acde07fbcdb6b840a6d4e5dd76cd871e178f36
downey <532720708@qq.com>
2020-01-07 14:34:50 +0800

    integerBreak

--- Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
+++ Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
@@ -361,6 +361,38 @@ public class Solution {
     }
 
 
+    /**
+     * p96
+     *
+     * @param n
+     * @return
+     */
+    public int integerBreak(int n) {
+//        if (n < 2) {
+//            return 0;
+//        }
+//        if (n == 2) {
+//            return 1;
+//        }
+//        if (n == 3) {
+//            return 2;
+//        }
+//        int timesOf3 = n / 3;
+//        if (n - timesOf3 * 3 == 1) {
+//            timesOf3--;
+//        }
+//        int timesOf2 = (n - timesOf3 * 3) / 2;
+//        return (int) (Math.pow(3, timesOf3)) * (int) (Math.pow(2, timesOf2));
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), dp[j] * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.movingCount(10, 1, 100));
