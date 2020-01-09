1b7c84b22d3ab0afd14b3dab7b32d31b6f65b8e8
downey <532720708@qq.com>
2020-01-09 20:22:14 +0800

    HasSubTree

--- Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
+++ Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
@@ -612,6 +612,33 @@ public class Solution {
         return head.next;
     }
 
+    /**
+     * p148
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+    }
+
+    private boolean isSubtreeWithRoot(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1.left, root2.left) && isSubtreeWithRoot(root1.right, root2.right);
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
     }
