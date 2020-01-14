8257ad6f23a89ba6820e4806666ac4b2ee682768
downey <532720708@qq.com>
2020-01-14 15:57:24 +0800

    Convert

--- Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
+++ Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
@@ -848,6 +848,12 @@ public class Solution {
         path.remove(path.size() - 1);
     }
 
+    /**
+     * p187
+     *
+     * @param pHead
+     * @return
+     */
     public RandomListNode Clone(RandomListNode pHead) {
         if (pHead == null) {
             return null;
@@ -874,6 +880,36 @@ public class Solution {
             cur = next;
         }
         return cloneHead;
+    }
+
+    /**
+     * p191
+     *
+     * @param pRootOfTree
+     * @return
+     */
+    private TreeNode pre = null;
+    private TreeNode head = null;
+
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        inOrder(pRootOfTree);
+        return head;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        node.left = pre;
+        if (pre != null) {
+            pre.right = node;
+        }
+        pre = node;
+        if (head == null) {
+            head = node;
+        }
+        inOrder(node.right);
     }
 
     public static void main(String[] args) {
