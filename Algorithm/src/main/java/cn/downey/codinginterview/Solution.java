284033bc9d5790a3a9ffcf36c76ff9f6aa147cb7
downey <532720708@qq.com>
2020-01-15 15:27:57 +0800

    Deserialize

--- Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
+++ Algorithm/src/main/java/cn/downey/codinginterview/Solution.java
@@ -918,8 +918,38 @@ public class Solution {
      * @param root
      * @return
      */
+    private String deserializeStr;
+
+    public String Serialize(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+        return root.val + " " + Serialize(root.left) + " " + Serialize(root.right);
+    }
+
+    public TreeNode Deserialize(String str) {
+        deserializeStr = str;
+        return Deserialize();
+    }
+
+    private TreeNode Deserialize() {
+        if (deserializeStr.length() == 0) {
+            return null;
+        }
+        int index = deserializeStr.indexOf(" ");
+        String node = index == -1 ? deserializeStr : deserializeStr.substring(0, index);
+        deserializeStr = index == -1 ? "" : deserializeStr.substring(index + 1);
+        if (node.equals("#")) {
+            return null;
+        }
+        TreeNode t = new TreeNode(Integer.parseInt(node));
+        t.left = Deserialize();
+        t.right = Deserialize();
+        return t;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        solution.Deserialize("1 2 3 4 # 5 6");
     }
 }
