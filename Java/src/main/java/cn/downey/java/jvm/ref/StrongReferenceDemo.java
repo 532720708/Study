4863ea850be8e3e729158144ce0635f8a42e2a63
downey <532720708@qq.com>
2020-01-16 16:20:56 +0800

    ReferenceDemo

---
+++ Java/src/main/java/cn/downey/java/jvm/ref/StrongReferenceDemo.java
@@ -0,0 +1,11 @@
+package cn.downey.java.jvm.ref;
+
+public class StrongReferenceDemo {
+    public static void main(String[] args) {
+        Object obj1 = new Object();
+        Object obj2 = obj1;
+        obj1 = null;
+        System.gc();
+        System.out.println(obj2);
+    }
+}
