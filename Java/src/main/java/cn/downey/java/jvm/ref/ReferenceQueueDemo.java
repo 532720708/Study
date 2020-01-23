98f9f7332b71d68542bbdad9e11d44f80cc08307
eDowney <zsj532720708@hotmail.com>
2020-01-23 21:36:38 +0800

    ReferenceQueueDemo

---
+++ Java/src/main/java/cn/downey/java/jvm/ref/ReferenceQueueDemo.java
@@ -0,0 +1,16 @@
+package cn.downey.java.jvm.ref;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+
+public class ReferenceQueueDemo {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        WeakReference<Object> weakReference = new WeakReference<>(o1, referenceQueue);
+
+        System.out.println(o1);
+        System.out.println(weakReference.get());
+        System.out.println(referenceQueue.poll());
+    }
+}
