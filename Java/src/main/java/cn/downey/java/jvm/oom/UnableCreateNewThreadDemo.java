bbd0e5dd2179b0886fa8f2e6f15cebcafdbf8a21
eDowney <zsj532720708@hotmail.com>
2020-01-31 17:30:30 +0800

    UnableCreateNewThreadDemo

--- Java/src/main/java/cn/downey/java/jvm/oom/UnableCreateNewThreadDemo.java
+++ Java/src/main/java/cn/downey/java/jvm/oom/UnableCreateNewThreadDemo.java
@@ -2,6 +2,15 @@ package cn.downey.java.jvm.oom;
 
 public class UnableCreateNewThreadDemo {
     public static void main(String[] args) {
-
+        for (int i = 1; ; i++) {
+            System.out.println("**************** i = " + i);
+            new Thread(() -> {
+                try {
+                    Thread.sleep(Integer.MAX_VALUE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, "" + i).start();
+        }
     }
 }
