29953d268510265e316d1c4c28c18a76ed7e3483
eDowney <zsj532720708@hotmail.com>
2020-02-01 20:04:11 +0800

    UnableCreateNewThreadDemo

---
+++ Java/src/main/java/cn/downey/java/jvm/oom/MetaSpaceOOMTest.java
@@ -0,0 +1,36 @@
+package cn.downey.java.jvm.oom;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MetaSpaceOOMTest {
+    static class OOMTest {
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        try {
+            while (true) {
+                i++;
+                Enhancer enhancer = new Enhancer();
+                enhancer.setSuperclass(OOMTest.class);
+                enhancer.setUseCache(false);
+                enhancer.setCallback(new MethodInterceptor() {
+                    @Override
+                    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                        return methodProxy.invokeSuper(o, args);
+                    }
+                });
+                enhancer.create();
+            }
+
+        } catch (Throwable e) {
+            System.out.println("===========" + i);
+            e.printStackTrace();
+        }
+    }
+}
